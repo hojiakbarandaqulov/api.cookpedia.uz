f26f64df171a42fb3b166b8c6d6bda622d94d871
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-03-11 21:59:08 +0500

    project settings

--- src/main/java/com/example/service/AuthService.java
+++ src/main/java/com/example/service/AuthService.java
@@ -1,8 +1,6 @@
 package com.example.service;
 
 import com.example.dto.ApiResponse;
-import com.example.dto.auth.LoginDTO;
-import com.example.dto.auth.ProfileDTO;
 import com.example.dto.auth.RegistrationDTO;
 import com.example.entity.ProfileEntity;
 import com.example.enums.AppLanguage;
@@ -10,17 +8,13 @@ import com.example.enums.GeneralStatus;
 import com.example.enums.RoleEnum;
 import com.example.exp.AppBadException;
 import com.example.repository.ProfileRepository;
-import com.example.util.JwtUtil;
-import com.example.util.MD5Util;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.repository.ProfileRoleRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.AllArgsConstructor;
-import org.springframework.cglib.core.Local;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Locale;
 import java.util.Optional;
 
 @Service
@@ -40,44 +34,23 @@ public class AuthService {
         Optional<ProfileEntity> profile = profileRepository.findByEmailAndVisibleTrue(dto.getEmail());
         if (profile.isPresent()) {
             ProfileEntity profileEntity = profile.get();
-            if (profileEntity.getStatus().equals(GeneralStatus.REGISTRATION)) {
-                profileRepository.delete(profileEntity);
-            } else {
+            if (profileEntity.getStatus().equals(GeneralStatus.REGISTRATION)){
+               profileRepository.delete(profileEntity);
+            }else {
                 throw new AppBadException(messageService.getMessage("profile.already.exists", language));
             }
         }
-        ProfileEntity entity = new ProfileEntity();
+        ProfileEntity entity=new ProfileEntity();
         entity.setFullName(dto.getFullName());
         entity.setEmail(dto.getEmail());
-        entity.setPassword(MD5Util.getMD5(dto.getPassword()));
+        entity.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
         entity.setStatus(GeneralStatus.REGISTRATION);
-        entity.setRoleEnum(RoleEnum.ROLE_USER);
         entity.setVisible(true);
         entity.setCreatedDate(LocalDateTime.now());
         profileRepository.save(entity);
 
-        mailSendingService.sendRegistrationEmail(dto.getEmail(), language);
-        return ApiResponse.ok(messageService.getMessage("registration.successful", language));
-    }
+        profileRoleService.create(entity.getId(), RoleEnum.ROLE_USER);
 
-
-    public ApiResponse<?> login(LoginDTO dto, AppLanguage language) {
-        Optional<ProfileEntity> loginProfile = profileRepository.findByEmailAndVisibleTrue(dto.getEmail());
-        if (loginProfile.isEmpty()) {
-            throw new AppBadException(messageService.getMessage("profile.password.wrong", language));
-        }
-        ProfileEntity entity = loginProfile.get();
-        if (!entity.getPassword().equals(MD5Util.getMD5(dto.getPassword()))) {
-            throw new AppBadException(messageService.getMessage("password.wrong", language));
-        }
-        if (!entity.getStatus().equals(GeneralStatus.ACTIVE)) {
-            throw new AppBadException(messageService.getMessage("profile.status.wrong", language));
-        }
-        ProfileDTO response = new ProfileDTO();
-        response.setFullName(entity.getFullName());
-        response.setEmail(entity.getEmail());
-        response.setRoleEnum(entity.getRoleEnum());
-        response.setJwt(JwtUtil.encode(response.getEmail(), entity.getId(), response.getRoleEnum()));
-        return ApiResponse.ok(response);
+        return ;
     }
 }
