9881990a4c64f76fa938d0293d8115dae0b86cbf
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-03-16 21:19:12 +0500

    Registration api Login apu finish

--- src/main/java/com/example/util/JwtUtil.java
+++ src/main/java/com/example/util/JwtUtil.java
@@ -54,14 +54,14 @@ public class JwtUtil {
         return new JwtDTO(id, username, roleLis);
     }*/
 
-    public static Integer decodeVerRegToken(String token) {
+    public static String decodeVerRegToken(String token) {
         Claims claims = Jwts
                 .parser()
                 .verifyWith(getSignInKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
-        return Integer.valueOf(claims.getSubject());
+        return String.valueOf(claims.getSubject());
     }
     private static SecretKey getSignInKey() {
         byte[] keyBytes = Base64.getUrlDecoder().decode(secretKey);
